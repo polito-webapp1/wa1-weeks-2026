3358fc42a8dabccd14d34f3ab93e49597d48c4b9
Fulvio Corno <fulvio.corno@polito.it>
2026-02-27 23:37:28 +0100

    comments and debug

--- week01/functions.js
+++ week01/functions.js
@@ -30,19 +30,34 @@ table_of_powers(5, fourth)
 table_of_powers(11, (x)=>{return Math.sqrt(x)})
 table_of_powers(11, x => Math.sqrt(x))
 
-// const person_1 = {
-//     first_name : 'Fulvio',
-//     last_name : 'Corno',
-//     address: {
-//         city: 'Turin',
-//         street: 'Corso Duca degli Abruzzi',
-//         number: 24,
-//         post_code: 10129
-//     },
-//     print_address : () => {
-//         console.log(this.address.street + " " + this.address.city)
-//     }
-// }
+const person_1 = {
+    first_name : 'Fulvio',
+    last_name : 'Corno',
+    address: {
+        city: 'Turin',
+        street: 'Corso Duca degli Abruzzi',
+        number: 24,
+        post_code: 10129
+    },
+    print_address : function() { 
+        // note: due to a "strange" behavior of the keyword 'this', it usage is applicable
+        // only if the function property is defined with the 'function' keyword and not
+        // as an arrow function (this was the bug we had in class) 
+        // more info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+        console.log(this.address.street + " " + this.address.city)
+    }
+}
+
+console.log(person_1)
+person_1.print_address()
+
+
+// Let's do the same with a Constructor Function.
+// Rules:
+// 1. the name of the function must be capitalized
+// 2. the function should initialize the fields of the object 'this' (automatically created as an empty object)
+// 3. no return statement
+// 4. the function must be called with the 'new' keyword (that actually creates 'this')
 
 function Person(name, last, city, street, number, postcode) {
     this.first_name = name
@@ -55,14 +70,15 @@ function Person(name, last, city, street, number, postcode) {
     }
 
     this.print_address = () => {
-        console.log(this.address.street + " " + this.address.city)
+        const a = this.address
+        console.log(`${a.street} ${a.number}, ${a.postcode} ${a.city}`)
     }
 
 }
 
-const person_1 = new Person("F", "C", "T", "D", 24, 10129)
+const person_1b = new Person("Fulvio", "Corno", "Turin", "Duca degli Abruzzi", 24, 10129)
 
-console.log(person_1)
-person_1.print_address()
+console.log(person_1b)
+person_1b.print_address()
 
 console.log()
--- week01/objects.js
+++ week01/objects.js
@@ -24,20 +24,25 @@ person_2.age = 'unknown'
 console.log(person_1)
 console.log(person_2)
 
+
+
 const names = person_1.first_name + " " + person_2.first_name
 const city1 = person_1.city ?? "Rome"
 const city2 = person_2.city ?? "Rome"
 // nullish coalescing operator (checks if the 1st argument is null
 // or undefined -- not for 0 or '')
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+
 
 const wc = person_1['work city']
 
 const my_city_1 = person_1.address.city
 const my_city_2 = person_2?.address?.city
+// Optional Chaining operator - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 
-const person_1_copy = { ...person_1 }
-const person_1_copy_a = { ...person_1, city:'Paris' }
-const person_1_copy_b = { city:'Paris', ...person_1 }
+const person_1_copy = { ...person_1 }  // copy of an object
+const person_1_copy_a = { ...person_1, city:'Paris' } // overrive a property value with an updated value (or create if it's missing)
+const person_1_copy_b = { city:'Paris', ...person_1 } // default value, if the property is NOT defined in object
 
 
 // person_1_copy = {}
@@ -48,10 +53,14 @@ const person_1_copy_b = { city:'Paris', ...person_1 }
 person_1_copy.city = 'Rome'
 person_1_copy.address.city ='New York'
 
-const phonebook = {}
+const phonebook = {}  // object as an associaive array
+
+// Note: for more serious associative data structures, use Map objects:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
 
 phonebook['Fulvio'] = 1234
 phonebook['Francesca'] = 9999
+
 
 console.log(Object.keys(phonebook))
 
