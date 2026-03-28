d21e5b69c89cb735f757054ba43a2d42fc5deaa0
Yasin-Rohani <yasinrohani89@gmail.com>
2026-03-28 12:05:23 +0100

    Update better-scores.js
    solve better scores exercise

--- week01/better-scores.js
+++ week01/better-scores.js
@@ -1,64 +1,82 @@
-"use strict" ;
+"use strict";
 
 /*
 # Exercise Basic-1: Better Scores
 _Goal: basic handling of JavaScript arrays_
 
-Develop a small JavaScript program to manage the scores given to your user on website . Scores are integer numbers, and they may be negative. You should:
- 
+Develop a small JavaScript program to manage the scores given to your user on website. Scores are integer numbers, and they may be negative. You should:
+
 - Define an array with all the scores you received in chronological order. For simplicity:
   - Embed the scores directly in the source code.
 - Duplicate the array, but:
-  - Eliminate all negative scores (call \`NN\` the number of negative scores that are deleted).
+  - Eliminate all negative scores (call `NN` the number of negative scores that are deleted).
   - Eliminate the two lowest-ranking scores.
   - Add `NN+2` new scores, at the end of the array, with a value equal to the (rounded) average of the existing scores.
 - Print both arrays, comparing the scores before and after the "improvement," and showing the averages in both cases.
 */
 
+const scores = [3, -2, 10, 0, -5, -8, 7, 4, -1, 9, 6];
 
-const scores = [3, -2, 10, 0, -5, -8, 7, 4, -1, 9, 6] ;
+const newscores = [];
 
-const newscores = []
-
+// count negative scores
+let NN = 0;
 for (const value of scores) {
-  if (value >= 0) {
-    newscores.push(value)
+  if (value < 0) {
+    NN++;
   }
 }
 
-for (const c of [1,2]) {
-  const smallest = Math.min(...newscores)
-  const pos_smallest = newscores.indexOf(smallest)
-  console.log("Smallest", smallest, "at position", pos_smallest)
-  console.log(`The smallest value is ${smallest} and is at position ${pos_smallest}`)
-  newscores.splice(pos_smallest,1)
+// copy only non-negative scores
+for (const value of scores) {
+  if (value >= 0) {
+    newscores.push(value);
+  }
 }
 
-let avg = 0.0
+// remove the two smallest scores
+for (const c of [1, 2]) {
+  const smallest = Math.min(...newscores);
+  const pos_smallest = newscores.indexOf(smallest);
+  console.log("Smallest", smallest, "at position", pos_smallest);
+  console.log(`The smallest value is ${smallest} and is at position ${pos_smallest}`);
+  newscores.splice(pos_smallest, 1);
+}
+
+// compute average of improved scores before adding new values
+let avg = 0.0;
 for (const value of newscores) {
-  avg += value
+  avg += value;
 }
-avg /= newscores.length
-avg = Math.round(avg)
+avg /= newscores.length;
+avg = Math.round(avg);
 
-console.log(avg)
+console.log("Rounded average before adding new scores:", avg);
 
-while( scores.length > newscores.length) {
-  newscores.push(avg)
+// add NN + 2 new scores
+for (let i = 0; i < NN + 2; i++) {
+  newscores.push(avg);
 }
 
+// compute original average
+let avgOriginal = 0.0;
+for (const value of scores) {
+  avgOriginal += value;
+}
+avgOriginal /= scores.length;
+avgOriginal = Math.round(avgOriginal);
 
-// const newscores = [...scores]  // make a copy
+// compute final improved average
+let avgImproved = 0.0;
+for (const value of newscores) {
+  avgImproved += value;
+}
+avgImproved /= newscores.length;
+avgImproved = Math.round(avgImproved);
 
-// for (let i = 0 ; i<newscores.length; i++ ) {
-//   const value = newscores[i]
-//   if(value<0) {
-//     newscores.splice(i, 1)
-//   }
-//   console.log(value)
-// }
-console.log(scores)
-console.log(newscores)
-
-// console.log(scores)
-
+// print results
+console.log("NN =", NN);
+console.log("Original scores:", scores);
+console.log("Original average:", avgOriginal);
+console.log("Improved scores:", newscores);
+console.log("Improved average:", avgImproved);
